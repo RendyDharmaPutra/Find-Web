3fe9318106679a7e8e85f23c88207fc4e58dac01
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-02-27 21:25:28 +0700

    add lucide-react dependenciy, update auth_sonner component

--- app/core/components/float/sonner/auth_sonner.tsx
+++ app/core/components/float/sonner/auth_sonner.tsx
@@ -1,5 +1,6 @@
 import { AuthActionType } from "~/features/auth/type/action/auth_action_type";
 import { SonnerWrapper } from "./sonner_wrapper";
+import { CircleAlert, CircleCheck, Info } from "lucide-react";
 
 type AuthSonnerProps<T extends AuthActionType> = {
   response: T | undefined;
@@ -13,12 +14,23 @@ export const AuthSonner = <T extends AuthActionType>(
 
   return (
     <SonnerWrapper condition={response}>
-      <div className="absolute top-8 right-8 px-4 py-1 flex flex-col justify-center items-center gap-1 min-w-[480px] min-h-[56px] rounded-medium bg-primaryBG">
-        <h6 className="font-medium text-lg text-secondaryTxt">
-          {response?.message}
-        </h6>
-        {error && <p className="text-sm text-tertiaryTxt">{error?.message}</p>}
-      </div>
+      <section className="absolute top-8 right-8 p-4 flex flex-row items-center gap-3 min-w-[360px] max-w-[480px] min-h-[56px] rounded-medium bg-primaryBG">
+        <div className="w-7 h-7 rounded-full ">
+          {response?.status === "Success" ? (
+            <CircleCheck className="w-full h-full text-success" />
+          ) : (
+            <CircleAlert className="w-full h-full text-danger" />
+          )}
+        </div>
+        <div className="flex flex-col justify-center gap-[3px] w-full h-full ">
+          <h6 className="font-semibold text-base text-primaryTxt w-full">
+            {response?.message}
+          </h6>
+          {error && (
+            <p className="text-sm text-tertiaryTxt w-full">{error?.message}</p>
+          )}
+        </div>
+      </section>
     </SonnerWrapper>
   );
 };
